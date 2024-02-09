01c7a4361bfa1d5db50ee469d1777f63e2b26f19
Kiminni <imk0980@gmail.com>
2024-02-09 14:28:27 +0900

    chore: 초기 세팅 완료

---
+++ src/main/java/com/example/tukgraduation/TukgraduationApplication.java
@@ -0,0 +1,13 @@
+package com.example.tukgraduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukgraduationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukgraduationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tukgraduation/TukgraduationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tukgraduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukgraduationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
