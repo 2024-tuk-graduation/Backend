2d7e5de5885f393b003ea271a35b5c66ca55900e
Kiminni <imk0980@gmail.com>
2024-03-14 00:24:59 +0900

    fix : 메시지 송신 경로 수정

--- src/main/java/com/example/tukgraduation/chatroom/controller/CodeController.java
+++ src/main/java/com/example/tukgraduation/chatroom/controller/CodeController.java
@@ -3,6 +3,7 @@ package com.example.tukgraduation.chatroom.controller;
 import com.example.tukgraduation.chatroom.dto.CodeMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -15,8 +16,9 @@ public class CodeController {
     }
     // Constructor omitted for brevity
 
-    @MessageMapping("/code")
-    public void handleCodeSubmission(@Payload("codeMessage")  CodeMessage codeMessage) {
+    @MessageMapping("code")
+    @SendTo("/pub/code")
+    public void handleCodeSubmission(CodeMessage codeMessage) {
         // 메시지를 받고, 처리한 뒤 다시 해당 워크스페이스 구독자에게 메시지를 전송합니다.
         messagingTemplate.convertAndSend("/sub/code", codeMessage);
     }
