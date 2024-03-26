7ce0491b836bbf1a38f6c1098f45aff6ddfda357
Kiminni <imk0980@gmail.com>
2024-03-26 21:54:01 +0900

    feat : ExceptionHandler 추가

---
+++ src/main/java/com/example/tukgraduation/global/error/ErrorCode.java
@@ -0,0 +1,27 @@
+package com.example.tukgraduation.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // Global
+    INTERNAL_SERVER_ERROR( "G001", 500, "예상치 못한 서버 내부 오류"),
+    EXTERNAL_LIBRARY_ERROR("GOO2", 400, "외부 라이브러리로 인해 예외가 발생했습니다."),
+    // Auth
+    AUTHORIZED_ERROR("A001", 400, "접근 권한이 없습니다. 로그인이 유효한지 확인해주세요"),
+    LOGIN_FAILED_ERROR("A002", 400, "아이디나 비밀번호가 잘못되었습니다"),
+    DUPLICATE_NICKNAME_ERROR("M001", 400, "이미 존재하는 닉네임입니다."),
+
+
+    ;
+    private final String code;
+    private final int status;
+    private final String message;
+
+    @Override
+    public String toString() {
+        return this.code + " : " + this.message;
+    }
+}
---
+++ src/main/java/com/example/tukgraduation/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.example.tukgraduation.global.error;
+
+import com.example.tukgraduation.global.advice.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @Order(0)
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse response = new ErrorResponse(errorCode);
+
+//        [ Error Level ]
+//        trace < debug < info < warn < error
+//        현재 logging level 은 info 로 설정되어, info, warn, error 만 로그에 남는다
+
+//        log.trace(e.toString());
+//        log.debug(e.toString());
+//        log.info(e.toString());
+//        log.warn(e.toString());
+        log.error(e.toString());
+
+        return ResponseEntity.status(errorCode.getStatus()).body(response);
+    }
+
+    @Order(1)
+    @ExceptionHandler
+    protected ResponseEntity<String> handleException(Exception e) {
+
+        log.error(ErrorCode.INTERNAL_SERVER_ERROR + ", " + Arrays.toString(e.getStackTrace()));
+
+        return ResponseEntity.status(500).body("서버 내부 예외적 에러 발생");
+    }
+}
