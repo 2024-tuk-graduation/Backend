f26046f057620ed075f08953fbc8a1b0911858c2
Kiminni <imk0980@gmail.com>
2024-04-01 18:49:03 +0900

    fix : 이미지 저장 로직 변경

--- src/main/java/com/example/tukgraduation/member/dto/AuthInfo.java
+++ src/main/java/com/example/tukgraduation/member/dto/AuthInfo.java
@@ -1,14 +1,15 @@
 package com.example.tukgraduation.member.dto;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 public class AuthInfo {
 
+    private Long id;
     private String username;
 
-    public AuthInfo(String username) {
+    public AuthInfo(String username, Long id) {
+        this.id = id;
         this.username = username;
     }
 }
