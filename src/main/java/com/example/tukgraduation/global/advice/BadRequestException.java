be1aad047168d2bf6445d730833167da62838348
Kiminni <imk0980@gmail.com>
2024-04-14 22:45:13 +0900

    fix : 오타 수정

--- src/main/java/com/example/tukgraduation/global/advice/BadRequestException.java
+++ src/main/java/com/example/tukgraduation/global/advice/BadRequestException.java
@@ -2,8 +2,8 @@ package com.example.tukgraduation.global.advice;
 
 import com.example.tukgraduation.global.error.ErrorCode;
 
-public class BadReqeustException extends BusinessException {
-    public BadReqeustException(ErrorCode errorCode) {
+public class BadRequestException extends BusinessException {
+    public BadRequestException(ErrorCode errorCode) {
         super(errorCode);
     }
 }
