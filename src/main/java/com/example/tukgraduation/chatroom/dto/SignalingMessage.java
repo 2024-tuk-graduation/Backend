da459792a3b3fa0661e8e40dd6c9fd3a3522717e
Kiminni <imk0980@gmail.com>
2024-05-08 20:27:18 +0900

    feat : sdpMid, sdpMLineIndex 추가 dto에 추가

--- src/main/java/com/example/tukgraduation/chatroom/dto/SignalingMessage.java
+++ src/main/java/com/example/tukgraduation/chatroom/dto/SignalingMessage.java
@@ -1,16 +1,24 @@
 package com.example.tukgraduation.chatroom.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SignalingMessage {
     private String type; // "offer", "answer", "iceCandidate"
     private String content; // JSON string of SDP or ICE candidate
     private String roomId; // Identifier of the chat room
+    private Integer sdpMLineIndex;
+    private String sdpMid;
 
-    public SignalingMessage(String type, String content, String roomId) {
+    public SignalingMessage(String type, String content, String roomId, Integer sdpMLineIndex, String sdpMid) {
         this.type = type;
         this.content = content;
         this.roomId = roomId;
+        this.sdpMLineIndex = sdpMLineIndex;
+        this.sdpMid = sdpMid;
     }
+
+
 }
