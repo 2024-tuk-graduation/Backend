3c7d46b3d1333acbe45b357656020e5725d365c9
Kiminni <imk0980@gmail.com>
2024-08-28 13:13:47 +0900

    fix: qna 수정

--- src/main/java/com/example/tukgraduation/chatroom/dto/QnaMessage.java
+++ src/main/java/com/example/tukgraduation/chatroom/dto/QnaMessage.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Optional;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class QnaMessage {
+    private String id; // unique identifier for each message
     private String type; // "question" or "answer"
     private String content;
+    private Optional<String> parentId = Optional.empty(); // Optional field to store the ID of the parent message (used only for answers)
 }
